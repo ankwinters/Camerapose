a9e83b5a86cf911cb2c867c361aac4dfb55131e0
ckpwinters <ckpwinters@gmail.com>
2017-03-16 12:53:05 +0800

    bug fix

--- demo/dataset_1/quaternion.py
+++ demo/dataset_1/quaternion.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
-from math import sin, cos, acos, sqrt
+from math import sin, cos, acos, sqrt, radians, degrees
 
 
 def normalize(v, tolerance=0.00001):
     mag2 = sum(n * n for n in v)
-    if abs(mag2 - 1.0) > tolerance:
+    if abs(mag2 - 1.0) > tolerance and mag2 != 0:
         mag = sqrt(mag2)
         v = tuple(n / mag for n in v)
+    #print("v:",v,"mag2:",mag2)
     return np.array(v)
 
 class Quaternion:
@@ -17,6 +18,8 @@ class Quaternion:
         theta = theta
         v = normalize(v)
 
+
+        print("v:",v,"theta:",theta)
         new_quaternion = Quaternion()
         new_quaternion._axisangle_to_q(theta, v)
         return new_quaternion
@@ -30,11 +33,13 @@ class Quaternion:
         x = v[0]
         y = v[1]
         z = v[2]
+# degrees to radians
+        th = radians(theta)
 
-        w = cos(theta/2.)
-        x = x * sin(theta/2.)
-        y = y * sin(theta/2.)
-        z = z * sin(theta/2.)
+        w = cos(th/2.)
+        x = x * sin(th/2.)
+        y = y * sin(th/2.)
+        z = z * sin(th/2.)
         self._val = np.array([w, x, y, z])
 
     def __mul__(self, b):
@@ -68,13 +73,15 @@ class Quaternion:
         result = Quaternion.from_value(np.array((w, -x, -y, -z)))
         return result
 
-    def __repr__(self):
+    def __str__(self):
         theta, v = self.get_axisangle()
-        return "((%.6f; %.6f, %.6f, %.6f))"%(theta, v[0], v[1], v[2])
+        return "((%.1f; %.6f, %.6f, %.6f))"%(theta, v[0], v[1], v[2])
 
     def get_axisangle(self):
         w, v = self._val[0], self._val[1:]
-        theta = acos(w) * 2.0
+        th = acos(w) * 2.0
+    # radians to degrees
+        theta = degrees(th)
 
         return theta, normalize(v)
 
@@ -84,4 +91,27 @@ class Quaternion:
     def vector_norm(self):
         w, v = self.get_axisangle()
         return np.linalg.norm(v)
+    def get_rotation_matrix(self):
+        w = self._val[0]
+        x = self._val[1]
+        y = self._val[2]
+        z = self._val[3]
+        wx = w * x 
+        wy = w * y
+        wz = w * z
+        xx = x * x
+        xy = x * y
+        xz = x * z
+        yy = y * y
+        yz = y * z
+        zz = z * z
+        """
+        [ 1 - 2(yy + zz)         2(xy - wz)          2(xz + wy)  ]
+        [      2(xy + wz)     1 - 2(xx + zz)         2(yz - wx)  ]
+        [      2(xz - wy)        2(yz + wx)     1 - 2(xx + yy) ]
+        """
+        return np.array([ [1-2*(yy+zz),2*(xy-wz),2*(xz+wy)],
+                          [2*(xy+wz),1-2*(xx+zz),2*(yz-wx)],
+                          [2*(xz-wy),2*(yz+wx),1-2*(xx+yy)] ]
+                          )
 
