65d0bf0b9816407c998e8049e233ca97c223e18e
ckpwinters <ckpwinters@gmail.com>
2017-04-05 18:52:09 +0800

    Fix camera pose calc error

--- demo/dataset_1/camera_pose_generator.py
+++ demo/dataset_1/camera_pose_generator.py
@@ -14,6 +14,8 @@ import re
 import os
 from quaternion import Quaternion
 
+from time import sleep
+
 
 def hand_trans(_loc):
     # TODO: We make it be the intermediate
@@ -23,29 +25,32 @@ def CamCirclePos(center,r,count=360,preci=8):
 # This function is an easy pose generator.The output forms as a circle in the plane y=y_center.
 # x^2+z^2=r^2
 # Output: cam[(x1,y,z1),(x2,y,z2),...,(xk,y,zk)] 
+# use parameterized (x,y,z) in right-handed system
+# change order
+# z = r*cos\theta
+# x = r*sin\theta
+# y = y0
+# 0<theta<2*pi
     random.seed()
     cam=[]
-    if count  % 2 != 0:
-        print("Error input count:", count)
-        exit(-1)
-# Determine the average gap between two sequence x-coordinates
-    k = count//2
-    gap = 2*abs(r)/k
-    for i in range(1,k+1):
-        #x = -r+gap*(i-1)+gap*random.random()
-        #x = -r+gap*(i-1)+gap*1
-        #y = center[1]
-        z = -r+gap*(i-1)+gap*random.random()
+# Initial cam  
+    cam.append(hand_trans( (round(0,preci),round(center[1],preci),round(r,preci)) ))
+    if count<1:
+        print("Num of count error.count should be at least 1")
+        return cam
+    elif count==1:
+        return cam
+    else:
+        cnt=count-1
+# Fill remaining cams
+    gap = 2*math.pi/count
+    for i in range(0,cnt):
+        #theta = random.random()*(i+1)/cnt*(2*math.pi)
+        theta = i*gap+(random.random()*0.5+0.5)*gap
+        z = r*math.cos(theta)
         y = center[1]
-        #print("z:",z)
-        x = math.sqrt(r*r-z*z)
-        cam.append((round(x,preci),round(y,preci),round(z,preci)))
-
-    for i in range(k,0,-1):
-        z = -r+gap*i-gap*random.random()
-        y = center[1]
-        x = -math.sqrt(r*r-z*z)
-        cam.append((round(x,preci),round(y,preci),round(z,preci)))
+        x = r*math.sin(theta)
+        cam.append(hand_trans( (round(x,preci),round(y,preci),round(z,preci)) ))
     
     return cam
 
@@ -62,7 +67,8 @@ def CamRandomPos(center,r,count=1,preci=8):
         theta = random.random()*math.pi*2
 # TODO: You need to make a better model.At present,phi is between pi/6 & 5*pi/6
         #phi = random.random()*math.pi
-        phi = (random.random()*0.5+1/18)*math.pi
+# [pi/6,pi/2)
+        phi = (random.random()*1/3+1/6)*math.pi
         z = center[0]+r*math.cos(theta)*math.sin(phi)
         x = center[1]+r*math.sin(theta)*math.sin(phi)
         y = center[2]+r*math.cos(phi)
@@ -93,7 +99,6 @@ def DeterminUpRight(loc,look_at,ratio=(4,3)):
 def DeterminDirect(cam,look_at):
     pass
 
-
 def SinglePoseTrans(loc, look_at, right):
 # It's based on right-handed coordinate system.
 # z-axis:vec = loc-look_at,
@@ -102,6 +107,7 @@ def SinglePoseTrans(loc, look_at, right):
 # fix:loc & look_at could be np.array
     if tuple(loc)==tuple(look_at):
         return "error"
+# t_vec+vec_=vec_in_world, _z=t_vec
     #_z = np.array(loc)-np.array(look_at)
     _z = hand_trans(np.array(loc)-np.array(look_at))
     _x = hand_trans(np.array(right))
@@ -115,40 +121,41 @@ def SinglePoseTrans(loc, look_at, right):
         if cos_theta>0.999:
             cos_theta = 1
         return math.degrees(math.acos(cos_theta))
-# Step1: Rotate z to _z, vec(0,0,1)->vec(cam_z)
+# Step1: Rotate from z to _z, that is vec(cam_z)(in world) -> (0,0,1)(in cam)
     theta_z = arccos( np.dot(cam_z,np.array((0,0,1))) )
     # if theta_z =180.0 or 0,then the axis would be either x or y
     rot_axis_z = np.array((1,0,0))
     if theta_z < 180.0 and theta_z > 0.01:
-        rot_axis_z = np.cross(np.array((0,0,1)),cam_z)
+        rot_axis_z = np.cross(cam_z,np.array((0,0,1)))
     # world_coordnate ->(0,0,1)(camera coordinate)
-    qz = Quaternion.from_axisangle(-theta_z,rot_axis_z)
+    qz = Quaternion.from_axisangle(theta_z,rot_axis_z)
     print("theta_z",theta_z," rot_axis_z",rot_axis_z," qz",qz)
-# Step2: Rotate x to _x,vec(1,0,0)-> Rz*vec(cam_x)
-    #cam_x_qz = qz*cam_x
+# Step2: Rotate x to _x,that is vec(cam_z)(in world) -> (1,0,0)(in cam)
+    world_x_qz = qz*cam_x
+    #cam_x = np.array((1,0,0))
     #print("camx:",cam_x," cam_x_qz:",cam_x_qz)
-    world_x_qz = qz*np.array((1,0,0))
+    #world_x_qz = qz*np.array((1,0,0))
     print("world_x_qz",world_x_qz,"np.dot:",np.dot(cam_x,world_x_qz))
-    theta_x = arccos( np.dot(cam_x, world_x_qz) )
+    #theta_x = arccos( np.dot(cam_x, world_x_qz) )
+    theta_x = arccos( np.dot(np.array((1,0,0)), world_x_qz) )
     # if theta_x =180.0,then the axis would be y or z
     rot_axis_x = np.array((0,1,0))
     if theta_x < 180.0 and theta_x > 0.01:
-        rot_axis_x = np.cross(world_x_qz,cam_x) 
+        rot_axis_x = np.cross(world_x_qz,np.array((1,0,0))) 
     # world_coordnate ->(1,0,0)(camera coordinate)
-    qx = Quaternion.from_axisangle(-theta_x,rot_axis_x)
+    qx = Quaternion.from_axisangle(theta_x,rot_axis_x)
     print("theta_x",theta_x," rot_axis_x",rot_axis_x," qx",qx)
 # Step3: Combine qz & qx
     q = qz*qx
     #print(q)
     #print(Quaternion.get_rotation_matrix(q))
     rotate = Quaternion.get_rotation_matrix(q)
-    # world_coordnate ->(0,0,0)(camera coordinate)
-    trans = -_z
+# bug fix:translation_vector transformation
+    trans = q*(-_z)
 # Output put R t
     rt = np.column_stack((rotate,trans))
     return np.reshape(rt,(1,12))
-    
-    #return np.hstack((rotate,trans.transpose()))
+
 
 def GenPovFile(template_file, cam_loc, direction, look_at, up, right, out_file_dir, out_file_name):
 #TODO: Change "direction" line
@@ -220,6 +227,7 @@ def GenCams(template_file,dst_dir, pose_file, pov_dir_name, img_dir_name,flag=0,
         cameras = CamRandomPos(look_at, distance, file_counts)
     count = 0
     rt_list = []
+    cur_dir = os.getcwd()
     for cam in cameras:
         up,right = DeterminUpRight(cam, look_at)
         print("cam:",cam," up:",up," right:",right)
@@ -246,9 +254,10 @@ def test():
 if __name__=="__main__":
     #cam = CamCircle((0,0,0),2,6)
     #DeterminRight(cam[0],(0,0,0))
-    #pose = SinglePoseTrans((0,0,1),(0,0,0),(0.707,0.707,0))
+    pose = SinglePoseTrans((1,0,-1),(0,0,0),(0.707,0,0.707))
+    print(pose)
     #new = []
-    test()
+    #test()
 
     #save_rt_paras(new,"/tmp/list.txt")
     #GenPovFile( "/tmp/gt.pov", (0,1,1), (3,4,5), (7,8,9), (9,10,11),"/tmp","1.pov")
